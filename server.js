b4dccef9947dcae0ff66e693c6cc20d73eddaad2
Ryan Alsobrooks <alsobrooks.ryan@gmail.com>
2023-09-20 17:35:23 -0400

    Add navlink and navbar components, modify styles dramatically

--- server.js
+++ server.js
@@ -13,25 +13,8 @@ app.get('/', (req, res) => {
     res.send(index());
 })
 
-app.get('/linkcard/:name/:url', (req, res) => {
-    console.log("Request to get linkcard: " + req.params.url + " " + req.params.name)
-
-    var card = pug.compileFile(`${root_directory}/views/components/linkcard.pug`);
-    res.send(card({name: req.params.name, url: req.params.url}));
-});
-
 app.get('/testurlmixin', (req,res) => {
     res.send("<div>Got url data!</div>");
-})
-
-app.get('/index.css', (req, res) => {
-    console.log("Sent css file")
-    res.sendFile('/assets/css/index.css', {root: root_directory});
-})
-
-app.get('/htmx.min.js', (req, res) => {
-    console.log("Sent htmx to client");
-    res.sendFile('htmx.min.js', {root: root_directory})
 })
 
 app.listen(port, () => {
