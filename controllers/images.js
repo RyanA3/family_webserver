8844b54ddf9ca2712267aa9e61c4f3dfc53879ba
Ryan Alsobrooks <alsobrooks.ryan@gmail.com>
2023-10-25 22:51:43 -0400

    Style sort, Add date taken, camera, and original name filter. Add page size. Style show duplicates checkbox. Add query form reset button.

--- controllers/images.js
+++ controllers/images.js
@@ -47,17 +47,29 @@ router.post('/upload', (req,res) => {
 
 })
 
-const page_size = 12;
+const DEFAULT_PAGE_SIZE = 12;
+const MIN_PAGE_SIZE = 1;
+const MAX_PAGE_SIZE = 100;
 var image_cards = pug.compileFile(`${root_directory}/views/components/image_cards.pug`)
 router.get('/images/:page', async (req, res) => {
 
+    //Parse query and options from request
     var page = req.params.page;
+    var pageSize = DEFAULT_PAGE_SIZE;
     var sort = { created: 'desc' }
     var filter = {};
-    if(req.query && req.query.sortBy) sort = { [req.query.sortBy]: req.query.sortDirection }
 
+    if(req.query && req.query.pageSize) {
+        if(req.query.pageSize < MIN_PAGE_SIZE) pageSize = MIN_PAGE_SIZE;
+        else if(req.query.pageSize > MAX_PAGE_SIZE) pageSize = MAX_PAGE_SIZE;
+        else pageSize = req.query.pageSize;
+    }
+    if(req.query && req.query.sortBy) {
+        sort = { 
+            [req.query.sortBy]: req.query.sortDirection 
+        }
+    }
     if(req.query && req.query.showDuplicates && req.query.showDuplicates == "no" || (!req.query.showDuplicates)) {
-        console.log("Not showing duplicates")
         filter = { 
             ...filter, 
             $or: [
@@ -65,24 +77,69 @@ router.get('/images/:page', async (req, res) => {
                 {duplicates: {$exists: false}},
                 {duplicates: []}
             ]
-        } 
-        
-    }   
+        }
+    }
 
-    console.log(req.query);
+    if(req.query && req.query.filterAfter && req.query.filterAfter !== "null") {
+        filter = {
+            ...filter,
+            created: {
+                $gte: req.query.filterAfter
+            }
+        }
+    }
 
+    if(req.query && req.query.filterBefore && req.query.filterBefore !== "null") {
+        filter = {
+            ...filter,
+            created: {
+                $lte: req.query.filterBefore
+            }
+        }
+    }
+
+    if(req.query && req.query.filterCamera && req.query.filterCamera !== "null") {
+        filter = {
+            ...filter,
+            $or: [
+                { 
+                    camera_make: {
+                        $regex: req.query.filterCamera,
+                    } 
+                },
+                { 
+                    camera_model: {
+                        $regex: req.query.filterCamera,
+                    } 
+                }
+            ]
+        }
+    }
+
+    if(req.query && req.query.filterOriginalName && req.query.filterOriginalName !== "null") {
+        filter = {
+            ...filter,
+            original_name: {
+                $regex: req.query.filterOriginalName,
+            }
+        }
+    }
+
+    //Do the query
     try {
         var metas = await ImageMeta.find(filter)
         .sort(sort)
-        .limit(page_size)
-        .skip(page * page_size)
+        .limit(pageSize)
+        .skip(page * pageSize)
         .select({ 
             _id: 1,
             file_size: 1,
             original_name: 1,
             created: 1,
             uploaded: 1,
-            extension: 1
+            extension: 1,
+            camera_make: 1,
+            camera_model: 1,
         })
 
         //Return empty response if nothing was found
@@ -93,34 +150,29 @@ router.get('/images/:page', async (req, res) => {
 
         var displayInfo = [metas.length];
 
-        //Format dates for display
+        //Format fields for display
         for(var i = 0; i < metas.length; i++) {
             displayInfo[i] = {
                 created: format(new Date(metas[i].created), "MMM do y"),
                 uploaded: format(new Date(metas[i].uploaded), "MMM do y"),
                 file_size: formatBytes(Number(metas[i].file_size)),
                 original_name: metas[i].original_name,
+                camera: metas[i].camera_make + " " + metas[i].camera_model,
             }
         }
 
-
-        console.log("Got images for client:\n " + metas);
-
-        //imageNames = metas[0]._id.toHexString() + "." + metas[0].extension
         var imageNames = [metas.length]
         
         for(var i = 0; i < metas.length; i++) {
             imageNames[i] = metas[i]._id.toHexString();
             if(metas[i].extension) imageNames[i] += "." + metas[i].extension
         }
-        console.log("\n\nGenerated names: " + imageNames + "\n\n")
 
         if(!is_production) image_cards = pug.compileFile(`${root_directory}/views/components/image_cards.pug`)
         res.status(200).send(image_cards({imageNames: imageNames, imageMetas: displayInfo}))
 
-
     } catch (e) {
-        console.error(e)
+        console.error("ERROR OCCURRED WHILE QUERYING IMAGES", e)
     }
 
 })
